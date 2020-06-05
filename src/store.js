7f67ca8b6168fc8eaf751652b20ad6460b6d4b45
brobaik <hhyungi94@gmail.com>
2020-06-05 15:14:21 +0900

    add createReducer

--- src/store.js
+++ src/store.js
@@ -1,41 +1,39 @@
 import { createStore } from 'redux';
+import { createAction, createReducer, configureStore } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
 const TODOS = 'TODOS';
 const getLoclItem = JSON.parse(localStorage.getItem(TODOS));
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+const addToDo = createAction('ADD');
+const deleteTodo = createAction('DELETE');
 
-const deleteTodo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
-const reducer = (state = getLoclItem, action) => {
-  switch (action.type) {
-    case ADD:
-      const addReturn = [{ text: action.text, id: Date.now() }, ...state];
-      return addReturn;
-    case DELETE:
-      const delReturn = state.filter((toDo) => action.id !== toDo.id);
-      return delReturn;
-    default:
-      return state;
-  }
-};
+// const reducer = (state = getLoclItem, action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       const addReturn = [{ text: action.payload, id: Date.now() }, ...state];
+//       return addReturn;
+//     case deleteTodo.type:
+// const delReturn = state.filter((toDo) => action.payload !== toDo.id);
+// return delReturn;
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer(getLoclItem, {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteTodo]: (state, action) => {
+    const delReturn = state.filter((toDo) => action.payload !== toDo.id);
+    return delReturn;
+  },
+});
+// const store = configureStore({ reducer });
 const store = createStore(reducer);
 const local = () => {
   const current = store.getState();
   localStorage.setItem(TODOS, JSON.stringify(current));
-  // const localStr = localStorage.getItem(TODOS);
-  // store.dispatch({type:default action})
 };
 
 store.subscribe(local);
